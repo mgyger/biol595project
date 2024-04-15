65681d5b06ced4dda668f3f23b83e3fc209e77b9
morga <mgyger@purdue.edu>
2024-04-15 12:45:15 -0400

    outline pdb code

---
+++ PDB.py
@@ -0,0 +1,13 @@
+"""---------------------------------------------------------------------------------------------------------------------
+PDB database pull code
+
+BIOL 595 Final Project
+Morgan Gyger 15 04 2024
+---------------------------------------------------------------------------------------------------------------------"""
+
+### need some way to pull ncbi outputs into string
+### for x in string
+### use PDB database to search the fasta
+### pull results into class? database?
+### use viewer code with like, for i in pdb: then in the viewer pdb[i] starting
+###     with i = 0
