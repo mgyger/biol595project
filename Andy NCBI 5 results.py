04eb8dbe961a7c6079978efb71112b12e1a036fc
dayekwon29 <156240161+dayekwon29@users.noreply.github.com>
2024-04-26 12:21:01 -0400

    Check sequence format added

--- Andy NCBI 5 results.py
+++ Andy NCBI 5 results.py
@@ -27,6 +27,13 @@ def run_ncbi_blast(sequence):
 
 def get_genomic_sequence_from_user():
     sequence = input("Enter the genomic sequence: ")
+
+    # Check if the sequence contains only valid nucleotides
+    if all(nucleotide in ['A', 'G', 'C', 'T'] for nucleotide in sequence.upper()):
+        return sequence
+    else:
+        print("Please input a sequence containing only A, G, C, or T nucleotides.")
+    
     return sequence
 
 
