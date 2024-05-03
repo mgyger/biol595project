003731451fb1526e75cbc488c85316bc0470b97d
Keck5 <156363883+Keck5@users.noreply.github.com>
2024-05-03 11:27:07 -0400

    PDB Process YIPPEE

--- Subprocessed Code.py
+++ Subprocessed Code.py
@@ -395,71 +395,77 @@ class InterProDataLoader: # InterProDataLoader done by DAYE KWON
 
             self.insert_into_database(table_name, table_data)
             print("Data inserted into the output_data database successfully.")
-class PDB: # PDB done by MORGAN GYGER
-    #### get top PDB ID matches for FASTA outputs ####
-    # get fasta from blast results
-    fasta_sequences = []
-    with open('flask_file/blast_result.txt', 'r') as file:
-        data = file.read()
+class PDB:
+    def __init__(self):
+        pass
 
-    # split the data
-    records = data.split('\n\n')
+    def fetch_top_identifiers(self):
+        # Get fasta from blast results
+        fasta_sequences = []
+        try:
+            with open('flask_file/blast_result.txt', 'r') as file:
+                data = file.read()
+                records = data.split('\n\n')
 
-    for record in records:
-        lines = record.split('\n')
-        for line in lines:
-            if line.startswith('Fasta Sequence:'):
-                fasta_sequence = line.split(': ')[1].strip()
-                fasta_sequences.append(fasta_sequence)
-                break
+                for record in records:
+                    lines = record.split('\n')
+                    for line in lines:
+                        if line.startswith('Fasta Sequence:'):
+                            fasta_sequence = line.split(': ')[1].strip()
+                            fasta_sequences.append(fasta_sequence)
+                            break
+        except FileNotFoundError:
+            print("Blast result file not found.")
+            return []
 
-    # set up to only get the first, closest result from pdb
-    top_identifiers = []
+        # Set up to only get the first, closest result from PDB
+        top_identifiers = []
 
-    # set up search query and define
-    for fasta_sequence in fasta_sequences:
-        query = {
-            "query": {
-                "type": "terminal",
-                "service": "sequence",
-                "parameters": {
-                    "evalue_cutoff": 1,
-                    "identity_cutoff": 0.9,
-                    "sequence_type": "protein",
-                    "value": fasta_sequence
-                }
-            },
-            "request_options": {
-                "scoring_strategy": "sequence"
-            },
-            "return_type": "entry"
-        }
+        # Set up search query and define
+        for fasta_sequence in fasta_sequences:
+            query = {
+                "query": {
+                    "type": "terminal",
+                    "service": "sequence",
+                    "parameters": {
+                        "evalue_cutoff": 1,
+                        "identity_cutoff": 0.9,
+                        "sequence_type": "protein",
+                        "value": fasta_sequence
+                    }
+                },
+                "request_options": {
+                    "scoring_strategy": "sequence"
+                },
+                "return_type": "entry"
+            }
 
-        # encode the query as JSON
-        encoded_query = json.dumps(query)
+            # Encode the query as JSON
+            encoded_query = json.dumps(query)
 
-        # construct the api url for searches of interest
-        api_url = "https://search.rcsb.org/rcsbsearch/v2/query?json=" + encoded_query
+            # Construct the API URL for searches of interest
+            api_url = "https://search.rcsb.org/rcsbsearch/v2/query?json=" + encoded_query
 
-        # set GET request to the API
-        response = requests.get(api_url)
+            # Set GET request to the API
+            response = requests.get(api_url)
 
-        # parse JSON response
-        json_response = response.json()
+            # Parse JSON response
+            json_response = response.json()
 
-        # extract the top identifier if match exists
-        top_identifier = None
-        if "result_set" in json_response and len(json_response["result_set"]) > 0:
-            top_identifier = json_response["result_set"][0]["identifier"]
+            # Extract the top identifier if match exists
+            top_identifier = None
+            if "result_set" in json_response and len(json_response["result_set"]) > 0:
+                top_identifier = json_response["result_set"][0]["identifier"]
 
-        # append top identifier to the list of all matches for all sequences
-        top_identifiers.append(top_identifier)
+            # Append top identifier to the list of all matches for all sequences
+            top_identifiers.append(top_identifier)
 
-    for i, top_identifier in enumerate(top_identifiers):
-        print(f"Top Identifier for sequence {i + 1}: {top_identifier if top_identifier else 'None'}")
+        for i, top_identifier in enumerate(top_identifiers):
+            print(f"Top Identifier for sequence {i + 1}: {top_identifier if top_identifier else 'None'}")
 
-    # fetch PDB content
-    def fetch_pdb_content(pdb_id):
+        return top_identifiers
+
+    def fetch_pdb_content(self, pdb_id):
         url = f'https://files.rcsb.org/download/{pdb_id}.pdb'
         try:
             response = requests.get(url)
@@ -469,12 +475,11 @@ class PDB: # PDB done by MORGAN GYGER
             print(f"Failed to fetch PDB content for {pdb_id}. Error: {e}")
             return None
 
-    # parse PDB content
-    def parse_pdb_content(pdb_content):
-        # create file-like object from the PDB content string
+    def parse_pdb_content(self, pdb_content):
+        # Create file-like object from the PDB content string
         pdb_file = StringIO(pdb_content)
 
-        # parse the PDB file
+        # Parse the PDB file
         parser = PDBParser()
         structure = parser.get_structure("pdb_data", pdb_file)
 
@@ -483,7 +488,7 @@ class PDB: # PDB done by MORGAN GYGER
             for chain in model:
                 for residue in chain:
                     for atom in residue:
-                        # convert float32 coordinates to regular floats
+                        # Convert float32 coordinates to regular floats
                         coord = [float(coord) for coord in atom.coord]
                         atoms.append({
                             "serial": atom.serial_number,
@@ -501,25 +506,12 @@ class PDB: # PDB done by MORGAN GYGER
 
         return atoms
 
-    # fetch PDB content for the first result (initial query)
-    first_pdb_id = top_identifiers[0] if top_identifiers else None
-    pdb_content = fetch_pdb_content(first_pdb_id)
-
-    # parse PDB content
-    if pdb_content:
-        atoms = parse_pdb_content(pdb_content)
-
-    else:
-        atoms = []
-
-    def write_pdb(atoms, filename):
+    def write_pdb(self, atoms, filename):
         with open(filename, 'w') as f:
             for atom in atoms:
                 pdb_line = f"ATOM  {atom['serial']:>5} {atom['name']:<4} {atom['residue']:>3} {atom['chain']:1} \
-    {atom['resSeq']:>4}    {atom['x']:>8.3f}{atom['y']:>8.3f}{atom['z']:>8.3f}{atom['occupancy']:>6.2f}{atom['tempFactor']:>6.2f}{atom['element']:>2}\n"
+{atom['resSeq']:>4}    {atom['x']:>8.3f}{atom['y']:>8.3f}{atom['z']:>8.3f}{atom['occupancy']:>6.2f}{atom['tempFactor']:>6.2f}{atom['element']:>2}\n"
                 f.write(pdb_line)
-
-    write_pdb(atoms, 'protein.pdb')
 
 
 
@@ -569,6 +561,8 @@ def main(): # main function compiled by ANDY KECK, MORGAN GYGER, DAYE KWON
 
     # Run NCBI BLAST search and retrieve top 5 results
     ncbi_blast.main(sequence)
+    pdb = PDB()
+    pdb.fetch_top_identifiers()
     sopma = SOPMA()
     sopma.conn = conn
     fasta_sequences = sopma.read_sequence_file('blast_result.txt')
